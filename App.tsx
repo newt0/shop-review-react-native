f95b39c88bc6fea58d09117e61e7fa1b5aab0c59
newt0 <py_lambda@yahoo.co.jp>
2020-12-22 15:30:15 +0900

    add: ShopReviewItems, Stars

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from "react-native";
 import "firebase/firestore";
 import { getShops } from "./src/lib/firebase";
 import { Shop } from "./src/types/shop";
+import ShopReviewItem from "./src/components/ShopReviewItem";
 
 const App = () => {
   const [shops, setShops] = useState<Shop[]>([]);
@@ -18,12 +19,22 @@ const App = () => {
 
   const shopItems = shops.map((shop, index) => (
     <View key={index.toString()}>
-      <Text>{shop.name}</Text>
-      <Text>{shop.place}</Text>
+      <ShopReviewItem shop={shop} />
     </View>
   ));
 
-  return <View style={styles.container}>{shopItems}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={shops}
+        renderItem={({ item }: { item: Shop }) => (
+          <ShopReviewItem shop={item} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={2}
+      />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
