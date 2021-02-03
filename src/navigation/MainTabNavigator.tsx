c22316543bc051f87a8ff87715483230ecb8d825
newt0 <py_lambda@yahoo.co.jp>
2021-02-03 12:59:30 +0900

    add: SearchScreen

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import UserScreen from "../screens/UserScreen";
 import HomeStackNavigator from "./HomeStackNavigator";
 import { Feather } from "@expo/vector-icons";
+import SearchScreen from "../screens/SearchScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,6 +19,16 @@ const MainTabNavigator = () => {
           tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="search"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
           ),
         }}
       />
---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState, useContext } from "react";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack/lib/typescript/src/types";
+import { RouteProp } from "@react-navigation/native";
+import { RootStackParamList } from "../types/navigation";
+
+type Props = {
+  navigation: StackNavigationProp<RootStackParamList, "Search">;
+  route: RouteProp<RootStackParamList, "Search">;
+};
+
+const SearchScreen: React.FC<Props> = ({ navigation, route }: Props) => {
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "flex-start",
+  },
+});
+
+export default SearchScreen;
